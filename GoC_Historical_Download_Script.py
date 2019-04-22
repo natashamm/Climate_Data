6e9a776b2fca35e47767ffa7fbd4fc5cf433242b
chowlet5 <howlett2306@rogers.com>
2019-04-22 18:39:40 -0400

    Added Downloading script

---
+++ GoC_Historical_Download_Script.py
@@ -0,0 +1,34 @@
+import pandas as pd
+import datetime
+from dateutil import rrule
+from datetime import datetime, timedelta
+from bs4 import BeautifulSoup
+import requests
+import re
+#from fuzzywuzzy import fuzz
+
+# Call Environment Canada API
+# Returns a dataframe of data
+def getHourlyData(stationID, year, month):
+    base_url = "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?"
+    query_url = "format=csv&stationID={}&Year={}&Month={}&timeframe=1".format(stationID, year, month)
+    api_endpoint = base_url + query_url
+    return pd.read_csv(api_endpoint, skiprows=15)
+
+
+
+stationID = 50093
+start_date = datetime.strptime('Mar2012', '%b%Y')
+end_date = datetime.strptime('Apr2019', '%b%Y')
+
+frames = []
+for dt in rrule.rrule(rrule.MONTHLY, dtstart=start_date, until=end_date):
+    df = getHourlyData(stationID, dt.year, dt.month)
+    frames.append(df)
+
+weather_data = pd.concat(frames)
+weather_data['Date/Time'] = pd.to_datetime(weather_data['Date/Time'])
+weather_data['Temp (°C)'] = pd.to_numeric(weather_data['Temp (°C)'])
+
+
+weather_data.to_csv('LondonA 2012-2019.csv')

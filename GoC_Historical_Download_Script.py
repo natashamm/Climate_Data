f12d5ad7d5bef87fe77a4edc11239dde3b3049fb
Natasha <natashamm04@gmail.com>
2019-09-30 12:45:20 -0700

    added input fields

--- GoC_Historical_Download_Script.py
+++ GoC_Historical_Download_Script.py
@@ -10,22 +10,30 @@ def getHourlyData(stationID, year, month):
     base_url = "http://climate.weather.gc.ca/climate_data/bulk_data_e.html?"
     query_url = "format=csv&stationID={}&Year={}&Month={}&timeframe=1".format(stationID, year, month)
     api_endpoint = base_url + query_url
-    return pd.read_csv(api_endpoint, skiprows=15)
+    return pd.read_csv(api_endpoint)
 
+#stationID = 50093
+stationID = input("Station ID: ")
+start_date = input("Start Month (mmmYYYY): ")
+#start_date = datetime.strptime('Mar2012', '%b%Y')
+start_date = datetime.strptime(start_date, '%b%Y')
+end_date = input("End Month (mmmYYYY): ")
+#end_date = datetime.strptime('Apr2019', '%b%Y')
+end_date = datetime.strptime(end_date, '%b%Y')
 
-
-stationID = 50093
-start_date = datetime.strptime('Mar2012', '%b%Y')
-end_date = datetime.strptime('Apr2019', '%b%Y')
-
+#make an empty list of all the dataframes
 frames = []
+#get each data frame for each month
 for dt in rrule.rrule(rrule.MONTHLY, dtstart=start_date, until=end_date):
     df = getHourlyData(stationID, dt.year, dt.month)
     frames.append(df)
 
+#combine monthly dataframes
 weather_data = pd.concat(frames)
+
+#format data frame
 weather_data['Date/Time'] = pd.to_datetime(weather_data['Date/Time'])
 weather_data['Temp (°C)'] = pd.to_numeric(weather_data['Temp (°C)'])
 
-
-weather_data.to_csv('LondonA 2012-2019.csv')
+#save
+weather_data.to_csv('station_%s_%s-%s.csv' % (stationID, start_date, end_date))

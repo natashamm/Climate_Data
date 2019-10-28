cc082e99406b056799bcc073a8c235b2af177c2b
Natasha <natashamm04@gmail.com>
2019-10-28 10:58:11 -0700

    remove and rename columns from final weather data file

--- weatherdata/download.py
+++ weatherdata/download.py
@@ -2,40 +2,45 @@
 import pandas as pd
 from dateutil import rrule
 from datetime import datetime
-#import weatherdata.helpers.get_distance as get_distance
 import weatherdata.helpers.weather_api as weather_api
-#import weatherdata.helpers.geographic as geographic
 
-#edmonton south campus
+# edmonton south campus
 stationID = 53718
 # !!! To Do: get closest station by address
 #address = input("Address:")
 #latlon = geographic.getLatLon(address)
 #stationID = geographic.getStationID(latlon[0], latlon[1])
 
-#stationID = input("Station ID: ")
+# stationID = input("Station ID: ")
 start_date = input("Start Month (mmmYYYY): ")
-#start_date = datetime.strptime('Mar2012', '%b%Y')
+# start_date = datetime.strptime('Mar2012', '%b%Y')
 start_date = datetime.strptime(start_date, '%b%Y')
 end_date = input("End Month (mmmYYYY): ")
-#end_date = datetime.strptime('Apr2019', '%b%Y')
+# end_date = datetime.strptime('Apr2019', '%b%Y')
 end_date = datetime.strptime(end_date, '%b%Y')
 
-#make an empty list of all the dataframes
+# make an empty list of all the dataframes
 frames = []
-#get each data frame for each month
+# get each data frame for each month
 for dt in rrule.rrule(rrule.MONTHLY, dtstart=start_date, until=end_date):
     df = weather_api.getHourlyData(stationID, dt.year, dt.month)
     frames.append(df)
 
-#combine monthly dataframes
+# combine monthly dataframes
 weather_data = pd.concat(frames)
 
-#format data frame
+# format data frame
 weather_data['Date/Time'] = pd.to_datetime(weather_data['Date/Time'])
 weather_data['Temp (°C)'] = pd.to_numeric(weather_data['Temp (°C)'])
 
-#save
+# take only relevant variables
+weather_data = weather_data[['Date/Time', 'Temp (°C)',
+							 'Dew Point Temp (°C)', 'Rel Hum (%)']]
+# rename columns
+weather_data = weather_data.rename(columns={"Temp (°C)": "Temp (degrees celsius)",
+										    "Dew Point Temp (°C)": "Dew Point Temp (degrees celsius)"})
+
+# save
 weather_data.to_csv('/Users/Natasha/Desktop/Climate_Data/downloads/station_%s_%s-%s.csv' % (stationID, start_date, end_date), index=False)
 
 

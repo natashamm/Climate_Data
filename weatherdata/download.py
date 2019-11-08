51b45dcca9a68fea858ec46ba92f6132961aa8c8
Natasha <natashamm04@gmail.com>
2019-11-08 10:48:30 -0800

    daily

--- weatherdata/download.py
+++ weatherdata/download.py
@@ -5,7 +5,9 @@ from datetime import datetime
 import weatherdata.helpers.weather_api as weather_api
 
 # edmonton south campus
-stationID = 53718
+#stationID = 53718
+# UofA
+stationID = 1801
 # !!! To Do: get closest station by address
 #address = input("Address:")
 #latlon = geographic.getLatLon(address)
@@ -23,22 +25,22 @@ end_date = datetime.strptime(end_date, '%b%Y')
 frames = []
 # get each data frame for each month
 for dt in rrule.rrule(rrule.MONTHLY, dtstart=start_date, until=end_date):
-    df = weather_api.getHourlyData(stationID, dt.year, dt.month)
+    df = weather_api.getDailyData(stationID, dt.year, dt.month)
     frames.append(df)
 
 # combine monthly dataframes
 weather_data = pd.concat(frames)
 
 # format data frame
-weather_data['Date/Time'] = pd.to_datetime(weather_data['Date/Time'])
-weather_data['Temp (°C)'] = pd.to_numeric(weather_data['Temp (°C)'])
+#weather_data['Date/Time'] = pd.to_datetime(weather_data['Date/Time'])
+#weather_data['Temp (°C)'] = pd.to_numeric(weather_data['Temp (°C)'])
 
 # take only relevant variables
-weather_data = weather_data[['Date/Time', 'Temp (°C)',
-							 'Dew Point Temp (°C)', 'Rel Hum (%)']]
+# weather_data = weather_data[['Date/Time', 'Temp (°C)',
+#							 'Dew Point Temp (°C)', 'Rel Hum (%)']]
 # rename columns
-weather_data = weather_data.rename(columns={"Temp (°C)": "Temp (degrees celsius)",
-										    "Dew Point Temp (°C)": "Dew Point Temp (degrees celsius)"})
+# weather_data = weather_data.rename(columns={"Temp (°C)": "Temp (degrees celsius)",
+#										    "Dew Point Temp (°C)": "Dew Point Temp (degrees celsius)"})
 
 # save
 weather_data.to_csv('/Users/Natasha/Desktop/Climate_Data/downloads/station_%s_%s-%s.csv' % (stationID, start_date, end_date), index=False)
